5855e649aa52429d9acf4dbeef925e8b2f891fea
corycalaway <cory.c.calaway@gmail.com>
2021-01-18 16:02:21 -0700

    added player data

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -6,6 +6,7 @@ console.log(new Potion());
 
 const Player = require('../lib/Player');
 
+// test for creation of player
 test('creates a player object', () => {
     const player = new Player('Dave');
   
@@ -16,4 +17,25 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
       );
+  });
+
+  // test players stats
+  test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  // test players infventory
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
   });
